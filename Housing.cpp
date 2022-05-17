acdb095ebaea5c5dbf9bd18d83c5bee5727949bc
Ashutosh Kumar Giri <93449692+ashutoshgiri182002@users.noreply.github.com>
2022-05-17 22:14:57 +0530

    Update Housing.cpp

--- Housing.cpp
+++ Housing.cpp
@@ -1,38 +1,80 @@
+/*
+Along one side of a road there is a sequence of vacant plots of land.
+Each plot has a diffrent area(non-zero).s0,the areas form a sequence a1,a2,..an.
 
+You want to buy K acres of land to build a house . you want to find all segment
+of continguous plots(i.e., a sbsection in the sequence) of whose sum is exactly
+K.
 
-// GIVEN A SENTANCE FIND OUT THE NO. OF ALPHABET,DIGTS AND namespace
+Sample input
+plots = [1 3 2 1 4 1 3 2 1 1 2]
+k = 8
 
-#include<bits/stdc++.h>
+sample output
+ 2 5
+ 4 6
+ 5 9
+*/
+
+#include<iostream>
 using namespace std;
+
+
+//MY way
+void housing(int *arr,int n, int k){
+
+  int i=0,j=0;
+  int  sum =0;
+  while(j<=n){
+    if(sum<k){
+      sum = sum + arr[j];
+      j++;
+    }
+    else if(sum > k){
+      sum = sum - arr[i];
+      i++;
+    }
+    else{
+      cout<<i<<" "<<j-1<<endl;
+      sum = sum + arr[j];
+      j++;
+    }
+  }
+
+
+}
+
+void Housing(int *arr,int n,int k){
+  int i=0;
+  int j = 0;
+  int cs = 0;
+
+  while(j<n){
+
+    //expand to right
+    cs +=arr[j];
+    j++;
+
+    while(cs>k and i<j){
+      cs = cs - arr[i];
+      i++;
+    }
+    //check if any given point
+    if(cs==k){
+      //print that window
+      cout<<i<<"-"<<j-1<<endl;
+    }
+  }
+  return;
+}
 
 int main(){
 
-  //read the sentance and count wich is required
+int plots[] = {1,3,2,1,4,1,3,2,1,1};
+int n = sizeof(plots)/sizeof(int);
 
-  char ch;
-  ch = cin.get();
+int k = 8;
+housing(plots,n,k);
 
-  int digits=0;
-  int alphabet=0;
-  int space=0;
-  while(ch!='\n'){
-    if(ch>='0' && ch<='9'){
-      digits++;
-    }
-
-    else if(ch>='a' and ch<='z'  or ch>='A' and ch<='Z'){
-      alphabet++;
-    }
-
-    else if(ch==' '){
-      space ++;
-    }
-
-    ch = cin.get();
-  }
-
-  cout<<"no. of digits "<<digits<<endl;
-  cout<<"no. of alphabet "<<alphabet<<endl;
-  cout<<"no. of space "<<space<<endl;
-  return 0;
+return 0;
 }
